8fc7c4b5688abaa6a4eb103478a3e039263713cd
Romilly Eveleigh <Romilly.Eveleigh@nttdata.com>
2022-01-06 16:17:55 +0000

    updated doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'NTT Dev Help',
+      title: 'NTT Dev Help 🤔',
       logo: {
         alt: 'My Site Logo',
         src: 'img/ntt-logo.png',
