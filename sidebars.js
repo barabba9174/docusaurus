402bf352532f30ca73edc066cb05680ee125ae9a
Pavitra-patil <pavitra.patil@nttdata.com>
2022-01-07 16:02:01 +0530

    New doc

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc4', 'doc1', 'doc2', 'doc3', ],
+    Docusaurus: ['doc5','doc4', 'doc1', 'doc2', 'doc3', ],
     Features: ['mdx'],
   },
 };
